02607595a258800a56685f6e03472affb83830f0
daniel <bro.danielx@gmail.com>
2019-01-08 14:52:42 -0500

    logging in progress

--- send_sms.py
+++ send_sms.py
@@ -1,8 +1,12 @@
+import logging
 from message import MESSAGE_BODY
 import os
 from foi import FOI_NUMBERS
+from pprint import pformat
 from twilio.rest import Client
 from twilio_variables import ACCOUNT_SID, AUTH_TOKEN, TWILIO_PHONE_NUMBER
+
+logging.basicConfig(level=logging.WARNING)
 
 
 account_sid = ACCOUNT_SID
@@ -16,11 +20,14 @@ to_numbers = FOI_NUMBERS
 
 def send(twilio_client, to_number, from_number, message):
     try:
-        twilio_client.messages.create(
+        message = twilio_client.messages.create(
             to = to_number,
             from_= from_number,
             body = message
         )
+
+        logging.warning(pformat(message.__dict__, indent=2))
+
     except Exception as e:
         print(e)
 
